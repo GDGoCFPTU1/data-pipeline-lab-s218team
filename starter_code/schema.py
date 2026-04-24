e82b546b63f9d4512458ca08a85cfd8030fdaaeb
dhhai <haicoin1324@gmail.com>
2026-04-24 12:15:10 +0700

    add files

--- starter_code/schema.py
+++ starter_code/schema.py
@@ -4,10 +4,17 @@ from pydantic import BaseModel, Field
 # ROLE 1: LEAD DATA ARCHITECT
 # ==========================================
 
+
 class UnifiedDocument(BaseModel):
     """
-    Hệ thống cần 6 trường thông tin chuẩn (document_id, source_type, author, category, content, timestamp). 
+    Hệ thống cần 6 trường thông tin chuẩn (document_id, source_type, author, category, content, timestamp).
     TODO: Khai báo các trường với kiểu dữ liệu str ở dưới.
     """
+
     # Khai báo các trường ở đây...
-    pass
+    document_id: str = Field(...)
+    source_type: str = Field(...)
+    author: str = Field(...)
+    category: str = Field(...)
+    content: str = Field(...)
+    timestamp: str = Field(...)
